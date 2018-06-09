911eb626f3776d1a5a2805d1128e485afae3f930
Alsein <xyh951115@live.com>
2018-06-09 15:43:31 +1000

    packing config update

--- src/Alsein.Algorithms.Searching/SearchAlgorithm.cs
+++ src/Alsein.Algorithms.Searching/SearchAlgorithm.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Alsein.Algorithms.Searching.SearchAlgorithms;
-using Alsein.Utilities;
 
 namespace Alsein.Algorithms.Searching
 {
